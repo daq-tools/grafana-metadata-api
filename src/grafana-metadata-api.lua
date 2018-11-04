8cf19601c7634206b7d67960705da50899f0ad6f
Andreas Motl <andreas@hiveeyes.org>
2018-11-04 16:36:49 +0100

    Allow GET requests, respond with 404 if not found

--- src/grafana-metadata-api.lua
+++ src/grafana-metadata-api.lua
@@ -38,37 +38,59 @@ if metadata_routes ~= nil then
 end
 
 -- Return early if request body empty, i.e. no sound POST request
+-- Compute designated target
+local target
+
+-- For non-body requests (e.g. GET), use target from URI
 if ngx.var.request_body == nil then
-    return 200
+    target = "luftdaten-stations.json"
+    -- ngx.log(ngx.ERR, "request_uri: ", ngx.var.request_uri)
+    -- ngx.log(ngx.ERR, "request_uri_prefix: ", ngx.var.request_uri_prefix)
+    target = ngx.var.request_uri:gsub(ngx.var.request_uri_prefix .. "/", "")
+
+-- For requests with bodies (e.g. POST), use target from JSON body
+else
+    -- Decode request body from JSON
+    local data = cjson.decode(ngx.var.request_body)
+
+    -- Use designated target from "target" attribute
+    target = data.target
+
+    --return 200
 end
 
--- Decode request body from JSON
-local data = cjson.decode(ngx.var.request_body)
 
 -- Add single route for dispatching by file names ending with .json
-local match, error = ngx.re.match(data.target, "^.+\\.json$", "ij")
+local match, error = ngx.re.match(target, "^.+\\.json$", "ij")
 if match then
     file_route = {
-        target = data.target,
-        uri    = "/" .. data.target,
+        target = target,
+        uri    = "/" .. target,
     }
     table.insert(routes, file_route)
 end
 
 -- Dispatch routes
 for i, route in pairs(routes) do
-    if ngx.re.match(data.target, route.target, "ij") then
-        res = ngx.location.capture(
+    if ngx.re.match(target, route.target, "ij") then
+        local response = ngx.location.capture(
             route.uri,
             { method = ngx.HTTP_GET }
         )
-        ngx.print(res.body)
-        ngx.status = 200
-        ngx.exit(ngx.OK)
+        -- ngx.log(ngx.ERR, target, route.target)
+        -- ngx.log(ngx.ERR, err)
+        -- ngx.log(ngx.ERR, "'" .. response.body .. "'")
+        if response.body ~= "" then
+            ngx.status = 200
+            ngx.print(response.body)
+            ngx.exit(ngx.OK)
+        end
     end
 end
 
 -- Routes did not match, so croak
-ngx.status = 400
-ngx.print("Bad Request: Target \"" .. data.target .. "\" not implemented")
+--ngx.status = 400
+--ngx.print("Bad Request: Target \"" .. target .. "\" not implemented")
+ngx.status = 404
+ngx.print("Resource not found: " .. target)
 ngx.exit(ngx.OK)
